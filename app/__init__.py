0702424a29d18953be4ff5ea554ff23b721be44c
fdsmora <fausto.ds.mora@gmail.com>
2020-09-19 13:43:43 -0500

    Fixed tests

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Flask
 
-def create_app():
+def create_app(test_conf=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,7 +1,8 @@
 import pytest
-from project import create_app
+from app import create_app
 
 def test_hello(client):
-    response = client.get("/hello")
-    print("RESPONSE:{}".format(response))
+    response = client.get("/hello/")
+    with open("dump.txt", "w") as f:
+        f.write(response.data.decode('utf-8'))
     assert response.data == b"Hello, World!"
