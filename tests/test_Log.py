6680bf38554257d115e2b94df2f47be2180caf73
fdsmora <fausto.ds.mora@gmail.com>
2020-09-20 14:33:16 -0500

    Moved check for test function

--- tests/test_Log.py
+++ tests/test_Log.py
@@ -1,10 +1,10 @@
-import os
 from app.db import get_db
 from app.model.Log import Log
-from tests.util import count_log_records_in_db, get_log_name, get_latest_filename_from_db
+from tests.util import count_log_records_in_db, get_log_name, get_latest_filename_from_db, check_test_logfile_exists
 
 def test_import_log_into_db(app, test_logfile):
     check_test_logfile_exists(test_logfile)
+
     with app.app_context():
         log = Log(test_logfile)
 
@@ -15,9 +15,7 @@ def test_import_log_into_db(app, test_logfile):
 
 def test_count_log_records(app, test_logfile):
     check_test_logfile_exists(test_logfile)
+
     with app.app_context():
         log = Log(test_logfile)
         assert log.get_records_count() == 7800
-
-def check_test_logfile_exists(test_logfile):
-    assert os.path.isfile(test_logfile) == True, "Test logfile '{}' must exist in order to run this test".format(test_logfile)
--- tests/util.py
+++ tests/util.py
@@ -1,3 +1,5 @@
+import os
+
 def get_log_name(logpath):
     return logpath.split('/')[-1]
 
@@ -8,3 +10,6 @@ def count_log_records_in_db(db):
 def get_latest_filename_from_db(db):
     row = db.execute('SELECT * FROM LOGFILE_NAMES').fetchone()
     return row['file_name']
+
+def check_test_logfile_exists(test_logfile):
+    assert os.path.isfile(test_logfile) == True, "Test logfile '{}' must exist in order to run this test".format(test_logfile)
