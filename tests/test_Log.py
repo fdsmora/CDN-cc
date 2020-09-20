a2d44afa217882c2f9f0f4de821e04b6d25149eb
fdsmora <fausto.ds.mora@gmail.com>
2020-09-20 13:45:10 -0500

    refactored import log into db test

--- tests/test_Log.py
+++ tests/test_Log.py
@@ -3,19 +3,14 @@ from app.model.Log import Log
 from tests.util import count_logfile_lines, count_lines, count_log_records_in_db
 
 def test_import_log_into_db(app, test_logfile):
-    logfile_name = test_logfile.split('/')[-1]
-
     with app.app_context():
-        db = get_db()
         log = Log(test_logfile)
 
-        file_name_from_db = get_latest_filename_from_db(db)
-        assert file_name_from_db == logfile_name 
+        db = get_db()
+        logfile_name = test_logfile.split('/')[-1]
 
-        total_file_lines = count_logfile_lines(test_logfile)
-        total_db_records = count_log_records_in_db(db) 
-
-        assert total_file_lines == total_db_records
+        assert get_latest_filename_from_db(db) == logfile_name 
+        assert count_logfile_lines(test_logfile) == count_log_records_in_db(db)
 
 def get_latest_filename_from_db(db):
     row = db.execute('SELECT * FROM LOGFILE_NAMES').fetchone()
